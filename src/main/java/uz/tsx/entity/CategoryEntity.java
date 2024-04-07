ba934867747f1856d8c518cf36fb304a3ed79cb2
bekzod1660 <bekzodsadriddinov454@gmail.com>
2024-04-07 14:36:35 +0500

    TODO DELETE UNIQUEs

--- src/main/java/uz/tsx/entity/CategoryEntity.java
+++ src/main/java/uz/tsx/entity/CategoryEntity.java
@@ -8,17 +8,32 @@ import org.springframework.beans.BeanUtils;
 import uz.tsx.constants.EntityStatus;
 import uz.tsx.constants.TableNames;
 import uz.tsx.dto.CategoryDto;
+import uz.tsx.entity.announcement.additionInfo.AdditionGroupEntity;
+import uz.tsx.entity.base.BaseEntity;
 import uz.tsx.entity.base.BaseForParentAndChild;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @Entity
 @Table(name = TableNames.CATEGORY)
-public class CategoryEntity extends BaseForParentAndChild {
+public class CategoryEntity extends BaseEntity {
+
+    @Column(name = "name_uz")
+    private String nameUz;
+
+    @Column(name = "name_ru")
+    private String nameRu;
+
+    @Column(name = "name_en")
+    private String nameEn;
+
+    @Column(name = "parentId")
+    private Long parentId;
 
     @OneToOne(cascade = CascadeType.ALL)
     private AttachEntity attach;
