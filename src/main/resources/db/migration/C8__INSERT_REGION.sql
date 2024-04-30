dad949a639e5d3a68cbbf9d6a99841f18be86228
bekzod1660 <bekzodsadriddinov454@gmail.com>
2024-05-01 01:06:49 +0500

    add file region sql and category attach check

---
+++ src/main/resources/db/migration/C8__INSERT_REGION.sql
@@ -0,0 +1,16 @@
+INSERT INTO tsx_region(status, created_date, name_en, name_ru, name_uz, parent_id)
+VALUES
+    ('CREATED', now(), 'Tashkent Sh.', 'Ташкент Ш.', 'Toshkent Shahar', NULL),
+    ('CREATED', now(), 'Samarkand', 'Самарканд', 'Samarqand', NULL),
+    ('CREATED', now(), 'Navoi', 'Навои', 'Navoiy', NULL),
+    ('CREATED', now(), 'Bukhara', 'Бухара', 'Buxoro', NULL),
+    ('CREATED', now(), 'Jizzakh', 'Джиззак', 'Jizzax', NULL),
+    ('CREATED', now(), 'Syr Darya', 'Сыр-Дарья', 'Sirdaryo', NULL),
+    ('CREATED', now(), 'Surkhandarya', 'Сурхандарья', 'Surxondaryo', NULL),
+    ('CREATED', now(), 'Kashkadarya', 'Кашкадарья', 'Qashqadaryo', NULL),
+    ('CREATED', now(), 'Khorezm', 'Хорезм', 'Xorazm', NULL),
+    ('CREATED', now(), 'Tashkent V.', 'Ташкент В.', 'Toshkent Viloyati', NULL),
+    ('CREATED', now(), 'Namangan', 'Наманган', 'Namangan', NULL),
+    ('CREATED', now(), 'Ferghana', 'Фергана', 'Fargʻona', NULL),
+    ('CREATED', now(), 'Andijan', 'Андижан', 'Andijon', NULL),
+    ('CREATED', now(), 'Karakalpak', 'Каракалпак', 'Qoraqalpogʻiston Respublikasi', NULL);
