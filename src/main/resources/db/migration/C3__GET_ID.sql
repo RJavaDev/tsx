2ea151cc60aa9cbdc8f888c1a997119a9247261a
bekzod1660 <bekzodsadriddinov454@gmail.com>
2024-05-02 09:03:29 +0500

    file region add district sql

--- src/main/resources/db/migration/C3__GET_ID.sql
+++ src/main/resources/db/migration/C3__GET_ID.sql
@@ -18,3 +18,14 @@ SELECT  id FROM tsx_add_group WHERE name_en = names OR name_ru = names;
 
 
 $$;
+
+CREATE OR REPLACE FUNCTION function_get_region_id(NAMES CHARACTER VARYING) RETURNS bigint
+    LANGUAGE SQL
+AS
+$$
+
+SELECT  id FROM tsx_region WHERE name_en = names OR name_ru = names;
+
+
+
+$$;
