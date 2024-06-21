b910690e9e0d711aa97c9c83cb9abe114165d008
RJavaDev <ravshanbek9918@gmail.com>
2024-06-22 00:00:51 +0500

    add images change package directory bash command

---
+++ into-img.sh
@@ -0,0 +1,4 @@
+
+#bu comanda images papkasini target ichiga solish uchun ishlatiladi
+#fileni jar fileni update qilgandan keyin ishlatiladi
+mv images target/
---
+++ release-img.sh
@@ -0,0 +1,4 @@
+
+#bu command target ichidagi images papkasini tashqariga olish uchun ishlatiladi
+#sababi jar fileni update qilgan paytda images papkasi o'chib ketadi
+mv target/images ./
